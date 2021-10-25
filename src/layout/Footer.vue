cc622d1df859a334e9a610a4d94a0f06df9f2781
naime-hossain <hossain.naime@yahoo.com>
2021-10-25 16:05:59 +0600

    pro version link and button updated

--- src/layout/Footer.vue
+++ src/layout/Footer.vue
@@ -1,23 +1,36 @@
 <script>
-export default {
-
-}
+export default {}
 </script>
 
 <template>
-  <footer class=" mt-10">
-    <div class="container-fluid mx-auto">
-      <BaseCard class="">
-        <div class="flex justify-between items-center">
-          <a target="_blank" href="https://aatrox-vue.netlify.app/" class="py-2 px-5 inline-block  bg-purple-500 text-white btn  rounded font-normal leading-4 ripple">Get Aatrox Pro</a>
-          <div>
-            <p>All rights reserved © UI Lib 2021</p>
-            
-
-          </div>
+    <footer class="mt-10">
+        <div class="container-fluid mx-auto">
+            <BaseCard class="">
+                <div class="flex justify-between items-center">
+                    <a
+                        target="_blank"
+                        href="https://aatrox-vue.netlify.app/"
+                        style="position: fixed;
+                         bottom: 14px; right: 80px"
+                        class="
+                            py-3
+                            px-5
+                            inline-block
+                            bg-green-500
+                            text-white
+                            btn
+                            rounded
+                            font-normal
+                            leading-4
+                            ripple
+                        "
+                        >Get Aatrox Pro</a
+                    >
+                    <div>
+                        <p>All rights reserved © UI Lib 2021</p>
+                    </div>
+                </div>
+            </BaseCard>
         </div>
-        
-      </BaseCard>
-    </div>
-  </footer>
+    </footer>
 </template>
